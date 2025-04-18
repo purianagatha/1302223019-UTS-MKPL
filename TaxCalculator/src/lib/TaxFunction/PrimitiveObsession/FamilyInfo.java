61796610dba7d064c82e5ec5168ae85439a97477
purianagatha <purianagatha@gmail.com>
2025-04-19 02:08:19 +0700

    mengubah parameter ke value object

---
+++ TaxCalculator/src/lib/TaxFunction/PrimitiveObsession/FamilyInfo.java
@@ -0,0 +1,9 @@
+public class FamilyInfo {
+    public boolean isMarried;
+    public int numberOfChildren;
+
+    public FamilyInfo(boolean isMarried, int numberOfChildren) {
+        this.isMarried = isMarried;
+        this.numberOfChildren = numberOfChildren;
+    }
+}
