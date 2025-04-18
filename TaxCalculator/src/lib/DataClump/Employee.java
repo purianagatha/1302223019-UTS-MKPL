8677c34c4d9c3678390dfa04872c381fd03ab299
purianagatha <purianagatha@gmail.com>
2025-04-19 01:47:51 +0700

    Mengubah variabel menjadi lebih ringkas

---
+++ TaxCalculator/src/lib/DataClump/Employee.java
@@ -0,0 +1,96 @@
+package lib;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Employee {
+
+	private String employeeId;
+	private String firstName;
+	private String lastName;
+	private String idNumber;
+	private String address;
+	
+	private LocalDate joinDate;
+	private int monthWorkingInYear;
+	
+	private boolean isForeigner;
+	private boolean gender; //true = Laki-laki, false = Perempuan
+	
+	private int monthlySalary;
+	private int otherMonthlyIncome;
+	private int annualDeductible;
+	
+	private Spouse spouse;
+
+	private List<Child> children;
+	
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.address = address;
+        this.joinDate = LocalDate.of(yearJoined, monthJoined, dayJoined);
+        this.isForeigner = isForeigner;
+        this.gender = gender;
+
+        children = new LinkedList<>();
+    }
+
+	
+	/**
+	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
+	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
+	 */
+	
+	public void setMonthlySalary(int grade) {	
+		if (grade == 1) {
+			monthlySalary = 3000000;
+			if (isForeigner) {
+				monthlySalary = (int) (3000000 * 1.5);
+			}
+		}else if (grade == 2) {
+			monthlySalary = 5000000;
+			if (isForeigner) {
+				monthlySalary = (int) (3000000 * 1.5);
+			}
+		}else if (grade == 3) {
+			monthlySalary = 7000000;
+			if (isForeigner) {
+				monthlySalary = (int) (3000000 * 1.5);
+			}
+		}
+	}
+	
+	public void setAnnualDeductible(int deductible) {	
+		this.annualDeductible = deductible;
+	}
+	
+	public void setAdditionalIncome(int income) {	
+		this.otherMonthlyIncome = income;
+	}
+	
+	public void setSpouse(String spouseName, String spouseIdNumber) {
+        this.spouse = new Spouse(spouseName, spouseIdNumber);
+    }
+
+	public void addChild(String childName, String childIdNumber) {
+        children.add(new Child(childName, childIdNumber));
+    }
+	
+	public int getAnnualIncomeTax() {
+		
+		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
+		LocalDate date = LocalDate.now();
+		
+		if (date.getYear() == joinDate.getYear()) {
+            monthWorkingInYear = date.getMonthValue() - joinDate.getMonthValue();
+        } else {
+            monthWorkingInYear = 12;
+        }	
+		    return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouse == null || spouse.isEmpty(), children.size());
+	}
+}
